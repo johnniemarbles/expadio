b2f561f639c5cae6e8948573a557a8059611deca
johnniemarbles <65386480+johnniemarbles@users.noreply.github.com>
2026-09-02 10:54:18 +0530

    feat(brand): route enterprise profile onboarding

---
+++ apps/brand-web/app/(workspace)/enterprise/onboard/profile/page.tsx
@@ -0,0 +1,9 @@
+import { BrandEnterpriseProfileOnboarding } from '../../../../components/BrandEnterpriseProfileOnboarding';
+import { resolveBrandContext } from '../../../../lib/brand-context';
+
+export const dynamic = 'force-dynamic';
+
+export default async function BrandEnterpriseProfileOnboardingPage() {
+  const context = await resolveBrandContext();
+  return <BrandEnterpriseProfileOnboarding subjectId={context.subjectId} />;
+}
