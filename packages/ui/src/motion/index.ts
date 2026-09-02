a2873168ff3996aa4b0570ad47fd66e8725dca42
johnniemarbles <65386480+johnniemarbles@users.noreply.github.com>
2026-09-02 20:17:23 +0530

    feat(ui): add motion component exports

---
+++ packages/ui/src/motion/index.ts
@@ -0,0 +1,6 @@
+export { MotionCard } from './MotionCard';
+export type { MotionCardProps } from './MotionCard';
+export { MotionPanel } from './MotionPanel';
+export type { MotionPanelProps } from './MotionPanel';
+export { MotionStatus } from './MotionStatus';
+export type { MotionStatusProps, MotionStatusTone } from './MotionStatus';
