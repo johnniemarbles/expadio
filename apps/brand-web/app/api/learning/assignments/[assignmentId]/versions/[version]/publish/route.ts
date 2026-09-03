f684b6f4fdfce7cce9ef7f75a1721e15a47d130f
johnniemarbles <65386480+johnniemarbles@users.noreply.github.com>
2026-09-04 00:31:19 +0530

    fix(brand): correct assignment publish context import

--- apps/brand-web/app/api/learning/assignments/[assignmentId]/versions/[version]/publish/route.ts
+++ apps/brand-web/app/api/learning/assignments/[assignmentId]/versions/[version]/publish/route.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto';
 import { NextResponse } from 'next/server';
 import { publishLearningAssignmentVersion } from '@expadio/postgres-runtime/learning-assignment';
-import { hasLearningAdmin, resolveBrandContext, withBrandTransaction } from '../../../../../../../../../lib/brand-context';
+import { hasLearningAdmin, resolveBrandContext, withBrandTransaction } from '../../../../../../../../lib/brand-context';
 
 const UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
