f0822abe05e6ec5016f44f20ca89fadd07b37fe4
Sanjeev Sood <sanjeevsood71@gmail.com>
2026-09-07 08:49:16 +0530

    chore: remove scratch files accidentally committed

--- scratch.cjs
+++
@@ -1,4 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config({ path: 'apps/platform-web/.env.local' });
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query('SELECT COUNT(*) FROM platform.agent_definitions').then(res => console.log('Definitions:', res.rows[0].count)).catch(e => console.error(e.message)).finally(() => pool.end());
